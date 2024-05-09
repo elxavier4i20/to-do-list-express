bace5f856a2ff052b31e977e3555a980719912be
Elson Xavier <elxaves83@gmail.com>
2024-05-09 19:49:28 -0300

    Enviando dados de um formulário com POST

--- src/routes/checklist.js
+++ src/routes/checklist.js
@@ -15,16 +15,26 @@ router.get('/', async (req, res) => {
     }
 })
 
+router.get('/new', async(req, res) => {
+    try {
+        let checklist = new Checklist();
+        res.status(200).render('checklists/new', { checklist: checklist });
+    } catch (error) {
+        res.status(500).render('pages/error', { errors: 'Erro ao carregar formulário.' })
+    }
+})
+
 // Criar coisas no Banco de Dados
 router.post('/', async (req, res) => {
-    let { name } = req.body
+    let { name } = req.body.checklist;
+    let checklist = new Checklist({name})
 
     try {
-        let checklists = await Checklist.create({ name })
-        res.status(200).json(checklists);
+        await checklist.save();
+        res.redirect('/checklists');
 
     } catch (error) {
-       res.status(422).json(error) 
+        res.status(422).render('checklists/new', { checklists: { ...checklist, error}})
     }
 })
 
@@ -34,7 +44,7 @@ router.get('/:id', async (req, res) => {
         let checklist = await Checklist.findById(req.params.id);
         res.status(200).render('checklists/show', { checklist: checklist});
     } catch (error) {
-        res.status(200).render('pages/error', {error: 'Erro ao exibir as Listas de Tarefas'})
+        res.status(500).render('pages/error', {error: 'Erro ao exibir as Listas de Tarefas'})
     }
 })
 
