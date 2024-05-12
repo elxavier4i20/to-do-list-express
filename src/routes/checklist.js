50c9f2d73c78b6de276e84f7047c6e9185f28fb5
Elson Xavier <elxaves83@gmail.com>
2024-05-11 21:17:58 -0300

    Atualização de dados com o PUT

--- src/routes/checklist.js
+++ src/routes/checklist.js
@@ -24,6 +24,15 @@ router.get('/new', async(req, res) => {
     }
 })
 
+router.get('/:id/edit', async(req, res) => {
+    try {
+        let checklist = await Checklist.findById(req.params.id);
+        res.status(200).render('checklists/edit', { checklist: checklist})
+    } catch (error) {
+        res.status(500).render('pages/error', {error: 'Erro ao exibir a edição de Listas de tarefas.'});
+    }
+})
+
 // Criar coisas no Banco de Dados
 router.post('/', async (req, res) => {
     let { name } = req.body.checklist;
@@ -49,13 +58,15 @@ router.get('/:id', async (req, res) => {
 })
 
 router.put('/:id', async (req, res) => {
-    let { name } = req.body
+    let { name } = req.body.checklist;
+    //let checklist = await Checklist.findById(req.params.id);
 
     try {
-        let checklist = await Checklist.findByIdAndUpdate(req.params.id, {name}, {new: true});
-        res.status(200).json(checklist)
+        await Checklist.findByIdAndUpdate(req.params.id, {name}, {new: true});
+        res.redirect('/checklists');
     } catch (error) {
-        res.status(422).json(error)
+        let errors = error.errors;
+        res.status(422).render('checklists/edit', {checklist: {...checklist, errors}});
     }
 })
 
