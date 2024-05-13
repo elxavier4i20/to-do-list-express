c74aa1126f1978ae980d3b3c8cb9784c233036dc
Elson Xavier <elxaves83@gmail.com>
2024-05-12 22:10:48 -0300

    Removendo dados com DELETE

--- src/routes/checklist.js
+++ src/routes/checklist.js
@@ -72,10 +72,10 @@ router.put('/:id', async (req, res) => {
 
 router.delete('/:id', async (req, res) => {
     try {
-        let checklist = await Checklist.findByIdAndRemove(req.params.id);
-        res.status(200).json(checklist)
+        let checklist = await Checklist.findByIdAndDelete(req.params.id);
+        res.redirect('/checklists');
     } catch (error) {
-        res.status(422).json(error)
+        res.status(500).render('pages/error', {error: 'Erro ao deletar as Listas de Tarefas'})
     }
 })
 
